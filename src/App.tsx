a576acf30cb75478984bbb0287431d540b4e7082
Samson Sim <samsonsim2@gmail.com>
2025-01-21 00:31:08 +0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { useCameraKit } from './hooks/useCameraKit';
+import { createMediaStreamSource, Transform2D } from '@snap/camera-kit';
+
+function App() {
+  const { session, lenses } = useCameraKit();
+  const canvasContainerRef = useRef<HTMLDivElement>(null);
+
+  const startCameraKit = useCallback(async () => {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+
+    const source = createMediaStreamSource(mediaStream, {
+      transform: Transform2D.MirrorX,
+    });
+
+    session.setSource(source);
+    session.applyLens(lenses[0]);
+    session.play('live');
+  }, [session, lenses]);
+
+  useEffect(() => {
+    startCameraKit();
+  }, [startCameraKit]);
+
+  useEffect(() => {
+    canvasContainerRef?.current?.replaceWith(session.output.live);
+  }, [session]);
+
+  return <div ref={canvasContainerRef}></div>;
+}
+
+export default App;
